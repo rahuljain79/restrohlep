829e2ecc82bffb7f88136b7caa6a395d925fd681
rahuljain79 <jainrahul7225@gmail.com>
2024-03-03 15:31:24 +0530

    add

--- app.component.ts
+++ app.component.ts
@@ -3,11 +3,14 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './pages/contact/contact.component';
+import { nopageComponent } from './pages/404/404.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent,  CommonModule,
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,  CommonModule,ContactComponent,nopageComponent,BlogComponent,
     RouterModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- app.routes.ts
+++ app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { nopageComponent } from './pages/404/404.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'product', component: ProductComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: '**', component: nopageComponent },
 ];
